3bdce217e7d3c5b89e20fc4e23c7c6033366923d
james grant <grantja92@gmail.com>
2020-12-03 19:41:15 +0000

    updated valregex to account to catch all matches

--- 2_password_philosophy.js
+++ 2_password_philosophy.js
@@ -1,10 +1,11 @@
-const passwordPhilosophy = (entries) => {
+const passwordPhilosophy = (inputString) => {
+  const entries = inputString.split("\n");
   let counter = 0;
   entries.forEach((entry) => {
     const password = entry.match(/(\w+)$/g)[0];
     const re = /(\d+)-(\d+) (\D)(?=:)(: .+)$/g;
-    const string = entry.replace(re, "^([^$3]*$3){$1,$2}$");
-    const valRegex = new RegExp(string);
+    const regexStr = entry.replace(re, "^([^$3]*$3[^$3]*){$1,$2}$");
+    const valRegex = new RegExp(regexStr, "g");
     if (valRegex.test(password) === true) {
       counter++;
     }
