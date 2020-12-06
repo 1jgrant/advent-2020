7b7dc85b4321462211058526d1279a5828d72823
james grant <grantja92@gmail.com>
2020-12-06 14:15:53 +0000

    refactored solution to parse binary

---
+++ 5/5_p1.js
@@ -0,0 +1,18 @@
+const boarding = (input) => {
+  const codes = input.split("\n");
+  const seatIds = codes.map((seat) => {
+    const positionStrings = [seat.slice(0, 7), seat.slice(7)];
+    const positionNums = positionStrings.map((pos) => {
+      const binary = pos.replace(/\w/g, (match) => {
+        if (match === "R" || match === "B") return "1";
+        return "0";
+      });
+      return parseInt(binary, "2");
+    });
+    const [row, col] = positionNums;
+    return row * 8 + col;
+  });
+  return Math.max(...seatIds);
+};
+
+module.exports = { boarding };
